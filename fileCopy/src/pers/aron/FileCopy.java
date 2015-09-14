fd49129b037ad8d93982d258cea276e9626a45e3
Aron <980253591@qq.com>
2015-09-14 23:03:02 +0800

    fileCopy

---
+++ fileCopy/src/pers/aron/FileCopy.java
@@ -0,0 +1,28 @@
+package pers.aron;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileCopy {
+
+	public static void main(String[] args) throws IOException{
+		File sourcefile = new File("./image/11.png");
+		String target = ".\\image" + File.separator + "file" +File.separator + sourcefile.getName();
+		File targetfile = new File(target);
+		/*if(!targetfile.exists()){   //²»ÐèÒª
+			System.out.println(targetfile.createNewFile());
+		}*/
+		byte[] data = new byte[2048];
+		FileInputStream input = new FileInputStream(sourcefile);
+		FileOutputStream output = new FileOutputStream(targetfile);
+		while(input.read(data) != -1){
+			output.write(data);
+		}
+				
+		input.close();
+		output.close();
+		
+	}
+}
